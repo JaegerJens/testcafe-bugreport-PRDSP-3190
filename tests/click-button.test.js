23ea7bc5d5a6857404abd417b45c25da300559fb
Jens Jäger <jens.jaeger@mobilex.eu>
2024-04-11 10:44:08 +0200

    extract nuvo html and css

--- tests/click-button.test.js
+++ tests/click-button.test.js
@@ -1,8 +1,8 @@
 import { Selector } from 'testcafe';
 
 fixture('Click submit button')
-    .page('https://jaegerjens.github.io/testcafe-bugreport-PRDSP-3190/static/')
+    .page('https://jaegerjens.github.io/testcafe-bugreport-PRDSP-3190/static/nuvo-extracted.html')
 
 test('click submit button', async t => {
-    await t.click(Selector('button[type=submit]').withExactText('Confirm'));
+    await t.click(Selector('button[type=submit]').withExactText('Confirm'))
 });
